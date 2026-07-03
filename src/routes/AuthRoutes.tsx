59225e6c865f7a4d77d7a778dbb78e0bd5698710
Vali127 <mh.valimbavaka@gmail.com>
2026-07-03 14:50:07 +0300

    feat : inscription refaite

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -1,11 +1,12 @@
 import { Route } from "react-router-dom";
 import AuthLayout from "../layouts/AuthLayout";
 import LoginForm from "../components/auth/LoginForm.tsx";
+import RegisterForm from "../components/auth/RegisterForm.tsx";
 
 const AuthRoutes = (
     <Route>
         <Route element={<AuthLayout />}>
-            <Route path="/register" element={<h1>Register Page</h1>} />
+            <Route path="/register" element={<RegisterForm/>} />
             <Route path="/login" element={<LoginForm/>} />
         </Route>
     </Route>
