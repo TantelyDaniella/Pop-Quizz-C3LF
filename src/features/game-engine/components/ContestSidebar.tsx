67b688b91d6fdd7341a5079e0f3d49bfb374ae84
Vali127 <mh.valimbavaka@gmail.com>
2026-07-29 05:26:04 +0300

    fix : critical hard coded props

--- src/features/game-engine/components/ContestSidebar.tsx
+++ src/features/game-engine/components/ContestSidebar.tsx
@@ -3,11 +3,13 @@ import { useContests, useJoinContest } from "@/features/game-engine/hooks/useCon
 import { useContestContext } from "@/features/game-engine/context/ContestContext";
 import ContestCard from "./ContestCard";
 import EmptyState from "@/components/common/EmptyState";
+import { getPlayerIdFromToken } from "@/app/utils/app.utils";
 
 export default function ContestSidebar() {
   const { contests } = useContests();
   const { join } = useJoinContest();
   const { joinedContest, setJoinedContest } = useContestContext();
+  const playerId = getPlayerIdFromToken();
 
   if (!contests.length)
     return <EmptyState icon={Inbox} text="Aucune partie disponible" height="h-40" />;
@@ -21,7 +23,7 @@ export default function ContestSidebar() {
                 isJoined={joinedContest?.id === contest.gameId}
                 hasJoinedAny={!!joinedContest}
                 onJoin={() => join(
-                    { gameId: contest.gameId, playerId: 20 },
+                    { gameId: contest.gameId, playerId: playerId ?? 0 },
                     { onSuccess: () => setJoinedContest({ id: contest.gameId, title: contest.title, totalQuestions: contest.totalQuestions }) }
                 )}
             />
